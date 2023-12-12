f088205b9e861064392a6ee7d6d995bcd3c2158b
Shashank Joshi <shashankjoshi368@gmail.com>
2023-12-11 22:16:14 -0500

    comit

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const authRoutes = require('./routes/authRoutes')
-const stripeRoutes = require('./routes/stripeRoutes')
+const authRoutes = require("./routes/authRoutes");
+const stripeRoutes = require("./routes/stripeRoutes");
 
 // express app
-const app = express()
+const app = express();
+
+//allow cors
+const cors = require("cors");
+app.use(
+  cors({
+    // credentials: true,
+    origin: "*",
+  })
+);
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 // routes
-app.use('/api', authRoutes)
-app.use('/api', stripeRoutes)
+app.use("/api", authRoutes);
+app.use("/api", stripeRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`Connected to DB & Listening on Port ${process.env.PORT}`)
-    })
+      console.log(`Connected to DB & Listening on Port ${process.env.PORT}`);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
